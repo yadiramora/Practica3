d6dc1932cad67661533205dee9e794d72735658d
yadiramora <almn.yadiracobos@cbtis66.edu.mx>
2022-10-12 19:39:08 -0500

    Practica3

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  contador = 1;
+
+  incrementar() {
+    this.contador++;
+  }
+
+  decrementar() {
+    this.contador--;
+  }
+
+}
